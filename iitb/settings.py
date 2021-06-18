73114c66371bd8c3968f542b113264ecb6c017fb
Shashank  Shahare <38653611+srshahare@users.noreply.github.com>
2021-06-19 00:36:19 +0530

    Update settings.py

--- iitb/settings.py
+++ iitb/settings.py
@@ -156,5 +156,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'luckyshahare678@gmail.com'
-EMAIL_HOST_PASSWORD = 'Lucky@678'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
